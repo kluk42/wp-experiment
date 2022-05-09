eac7b12cb22825f32e6ad1fc78cdde1210a58871
Nikita Nemykin <nikitanemykin@mail.ru>
2022-05-09 16:05:31 +0700

    Add dist to .gitignore

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,9 +1,11 @@
+const webpack = require('webpack');
+
 module.exports = {
-  mode: "production",
-  devtool: "hidden-source-map",
+  mode: 'production',
+  devtool: 'hidden-source-map',
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.name": JSON.stringify("Cool hacker"),
+      'process.env.name': JSON.stringify('Cool hacker'),
     }),
   ],
 };
